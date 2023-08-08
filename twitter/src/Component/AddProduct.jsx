8fd0cf1842cfe2fb028909d32398bd939662cab7
samiksha-3010 <samiksha30102@gmail.com>
2023-08-09 00:29:48 +0530

    updates

--- twitter/src/Component/AddProduct.jsx
+++ twitter/src/Component/AddProduct.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid';
 
 const AddProduct = () => {
     const [productData,setProductData] = useState ({name: "",price: "",image: "",Cetegries: "Others" })
-    const 
+     const  router = useNavigate();
 
     const handleChange = (event) =>{
         setProductData({...productData,[event.target.name]: event.target.value})  
@@ -12,14 +13,34 @@ const AddProduct = () => {
    event.preventDefault();
    if(productData.name && productData.price && productData.image && productData.Cetegries){
     const productArray = JSON.parse(localStorage.getItem("Products")) || []
-
+     const randomId = uuidv4
+     productData ["id"] = randomId
     productArray.push(productData)
     localStorage.setItem("Products", JSON.stringify(productArray))
     setProductData ({name: "",price: "",image: "",Cetegries: "Others" })
-    router("")
+    router("all-product")
+    alert ("Product added success")
 
+   }else{
+    alert("please fill all field")
    }
     }
+    function selectRole (event) {
+        console.log(event.target.value, "-role")
+        setProductData({...productData,["Cetegries"]: event.target.value })
+    }
+    useEffect(()=>{
+        const user = JSON.parse(localStorage.getItem("Currrent-user"))
+        if(user){
+            if(user?.role == "Buyer"){
+                alert("you are not a seller")
+                router('/')
+            }
+        }else{
+            alert("you are not a logged user")
+            router('/login')
+        }
+    },[])
 
   return (
     <div>
@@ -30,7 +51,7 @@ const AddProduct = () => {
             <lable> Product Price :</lable><br/>
             <input  value={productData.price} type='number' onChange={handleChange} name='price'/><br/>
             <label> Product Cetegries :</label><br/>
-            <select>
+            <select  onChange={selectRole}>
                 <option  value="Others">Others</option>
                 <option value="Womens">Womens</option>
                 <option value="Children">Children</option>
