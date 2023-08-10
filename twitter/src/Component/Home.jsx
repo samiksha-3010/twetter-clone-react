864d3981c1ee0d92e5af5f4193bf552630dd3c74
samiksha-3010 <samiksha30102@gmail.com>
2023-08-10 12:35:21 +0530

    updates

--- twitter/src/Component/Home.jsx
+++ twitter/src/Component/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+
+      <img style={{width: "60%",height: "400px"}} src='https://tse1.mm.bing.net/th?id=OIP.ci1E7m8wCISyHZ6um8z01gHaDt&pid=Api&rs=1&c=1&qlt=95&w=240&h=120'/>
+    </div>
   )
 }
 
--- twitter/src/Component/Navbar.jsx
+++ twitter/src/Component/Navbar.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import {AuthContext} from'./Context/AuthContext'
 
 const Navbar = () => {
+  const router = useNavigate();
+  const {state,Login,Logout} = useContext(AuthContext)
+  const [user, setUser ] = useState  ({});
+
+  useEffect(() =>{
+    if(state?. user){
+      setUser(state?. user)
+    }else{
+      setUser({})
+    }
+  },[state])
+ 
+
   return (
-    <div>Navbar</div>
+    <div  style={{width: "100%", height: "80px", border: "1px solid black", backgroundColor:"black", color:"white", display:"flex",justifyContent: "space-around"}}>
+     
+      <h2 onClick={() => router ('/')}>Navbar</h2>
+    <div>
+      {user?.email?  
+      <>
+    <h2  onClick={()=>router ('/addproduct')}>AddProduct</h2>
+    <h2>All Product</h2>
+    <h2>Cart</h2>
+    <h2>Logout</h2>
+    </>
+    :
+    <h2  onClick={() => router ('/register')}>Login</h2>
+  }
+    </div>
+    </div>
+      
+   
   )
 }
 
